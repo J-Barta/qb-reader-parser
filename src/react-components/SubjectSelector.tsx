cae28ca816acb9d921f46b855ee75070fd606247
Jacob Barta <jacob.barta25@gmail.com>
2023-08-31 13:51:24 -0400

    Create category expand and collapse
    Took 15 minutes

--- src/react-components/SubjectSelector.tsx
+++ src/react-components/SubjectSelector.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import "../../styles.css"
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {categories, Category} from "../Categories";
 import {QBReaderSettings} from "../../main";
 import {SubjectButton} from "./ui/SubjectButton";
@@ -54,66 +54,77 @@ export function SubjectSelector(props: {
 		props.upliftActiveSubcats(activeSubcats)
 	}, [activeSubcats])
 
+	const [categorySelectorActive, setCategorySelectorActive] = useState(false)
+
 	return (
-		<div className={"flex test-class-name"}>
+		<div>
+			<button
+				onClick={() => setCategorySelectorActive(!categorySelectorActive)}
+				className={categorySelectorActive ? "mod-cta" : ""}
+			>
+				Categories
+			</button>
 
-			<div className={"category-flex"}>
-				<h2>Categories</h2>
+			<div className={`flex cat-selector-container ${!categorySelectorActive ? "shrink-cat-select" : ""}`}>
 
-				{
-					categories.map(e => {
-						const active = activeCategories.includes(e.name)
+				<div className={"category-flex"}>
+					<h2>Categories</h2>
 
-						return <SubjectButton
-							key={e.name}
-							text={e.name}
-							color={e.color}
-							settings={props.settings}
-							onClick={() => {
+					{
+						categories.map(e => {
+							const active = activeCategories.includes(e.name)
 
-								if(!active) {
-									activeCategories.push(e.name)
+							return <SubjectButton
+								key={e.name}
+								text={e.name}
+								color={e.color}
+								settings={props.settings}
+								onClick={() => {
 
-									setActiveCategories([...activeCategories])
-								} else {
-									setActiveCategories(activeCategories.filter(ele => ele !== e.name))
-								}
+									if(!active) {
+										activeCategories.push(e.name)
 
-							}}
-							active={activeCategories.includes(e.name)}/>
+										setActiveCategories([...activeCategories])
+									} else {
+										setActiveCategories(activeCategories.filter(ele => ele !== e.name))
+									}
+
+								}}
+								active={activeCategories.includes(e.name)}/>
+						}
+						)
 					}
-					)
-				}
+				</div>
+
+				<div className={"category-flex"}>
+					<h2>Subcategories</h2>
+
+					{
+						availableSubcats.map(e => {
+							const active = activeSubcats.includes(e.name)
+
+							return <SubjectButton
+								key={e.name}
+								text={e.name}
+								color={e.color}
+								settings={props.settings}
+								onClick={() => {
+
+									if(!active) {
+										activeSubcats.push(e.name)
+
+										setActiveSubcats([...activeSubcats])
+									} else {
+										setActiveSubcats(activeSubcats.filter(ele => ele !== e.name))
+									}
+
+								}}
+								active={activeSubcats.includes(e.name)}/>
+						})
+					}
+				</div>
+
 			</div>
-
-			<div className={"category-flex"}>
-				<h2>Subcategories</h2>
-
-				{
-					availableSubcats.map(e => {
-						const active = activeSubcats.includes(e.name)
-
-						return <SubjectButton
-							key={e.name}
-							text={e.name}
-							color={e.color}
-							settings={props.settings}
-							onClick={() => {
-
-								if(!active) {
-									activeSubcats.push(e.name)
-
-									setActiveSubcats([...activeSubcats])
-								} else {
-									setActiveSubcats(activeSubcats.filter(ele => ele !== e.name))
-								}
-
-							}}
-							active={activeSubcats.includes(e.name)}/>
-					})
-				}
-			</div>
-
 		</div>
 	);
 }
